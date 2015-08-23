c10111c56e4cf7f5a57536a24511d6fa483de503
mattfinner <mattfinner@hotmail.com>
2015-08-23 21:39:10 +0100

    fixed find and replace

--- confignetwork.sh
+++ confignetwork.sh
@@ -6,10 +6,10 @@
 
 #set host name
 echo $1 > /etc/hostname
-sed -i "s/TEMPLATE/'$1'/g" /etc/hosts
+sed -i "s/TEMPLATE/$1/g" /etc/hosts
 
 #change ipaddress
-sed -i "s/'$2'/'$3'/g" /etc/network/interfaces
+sed -i "s/$2/$3/g" /etc/network/interfaces
 
 #set additional DNS
-sed -i "s/dns-nameserver 8.8.8.8 8.8.4.4/dns-nameserver 8.8.8.8 8.8.4.4 '$4'/g" /etc/network/interfaces
+sed -i "s/dns-nameserver 8.8.8.8 8.8.4.4/dns-nameserver 8.8.8.8 8.8.4.4 $4/g" /etc/network/interfaces
